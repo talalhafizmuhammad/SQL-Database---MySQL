f488e4e81046ddb00a9b83d981e214f847668d5a
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-08-12 07:23:45 +0500

    Create 01. Create'n'UseDatabase.sql

---
+++ 01. Create'n'UseDatabase.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE Syntax
+CREATE DATABASE database_name;
+
+-- Example
+CREATE DATABASE IF NOT EXISTS MyDataBase01;
+
+-- USE Database Syntax
+USE database_name;
+
+-- Example
+USE MyDataBase01;
