7598d9a536abf18e3d70bac70fa99f307fac3306
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-08-17 02:55:49 +0500

    Update 08. DeletingData.sql

--- 08. DeletingData.sql
+++ 08. DeletingData.sql
@@ -15,3 +15,8 @@
  -- Drop the Entire Table (use with caution, since it will DELETE the whole table)
  DROP TABLE users;
 
+-- BEST PRACTICES WHILE DELETION OF DATA:
+   -- Always use WHERE unless and untill you are intentionally deleting the data.
+   -- Consider running a SELECT with the same WHERE clause to ensure which rows are you going to affect.
+SELECT * FROM users WHERE id = 3;
+   -- Always back up important data before performing destructive operations.
